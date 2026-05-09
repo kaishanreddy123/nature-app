360a826029a792b5174111388d8c5a6731816aed
krishna.python786 <krishna.python786@gmail.com>
2026-05-09 12:19:29 +0530

    first deploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
-// Import Firebase core
+// src/firebase.js
+
 import { initializeApp } from "firebase/app";
 
-// Import Auth
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+  getAuth,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 import { getFirestore } from "firebase/firestore";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyD0lHwpw2_2yEPixCmj4ELC2oR8XRiia4w",
@@ -15,15 +16,13 @@ const firebaseConfig = {
   storageBucket: "natureapp786.firebasestorage.app",
   messagingSenderId: "182449328882",
   appId: "1:182449328882:web:8595824b99faefbe0e766b",
-  measurementId: "G-F6VQF5PTDM"
+  measurementId: "G-F6VQF5PTDM",
 };
 
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth
 export const auth = getAuth(app);
 
-// Google Provider
 export const provider = new GoogleAuthProvider();
 
 export const db = getFirestore(app);
